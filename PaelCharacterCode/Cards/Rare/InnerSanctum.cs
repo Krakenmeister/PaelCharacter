7107fcaf5eb8fdd4bf242543f50c25b44d1afee2
Krakenmeister <johndmeo@gmail.com>
2026-07-04 23:32:19 -0700

    Added Inner Sanctum card

---
+++ PaelCharacterCode/Cards/Rare/InnerSanctum.cs
@@ -0,0 +1,28 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using PaelCharacter.PaelCharacterCode.Powers;
+
+namespace PaelCharacter.PaelCharacterCode.Cards.Rare;
+
+public class InnerSanctum() : PaelCharacterCard(3,
+    CardType.Skill, CardRarity.Rare,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new ("InnerSanctumPower", 1)
+    ];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CommonActions.ApplySelf<InnerSanctumPower>(choiceContext, this);
+        PlayerCmd.EndTurn(Owner, false);
+    }
+
+    protected override void OnUpgrade() => EnergyCost.UpgradeBy(-1);
+}
---
+++ PaelCharacterCode/Powers/InnerSanctumPower.cs
@@ -0,0 +1,20 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+
+namespace PaelCharacter.PaelCharacterCode.Powers;
+
+public class InnerSanctumPower() : PaelCharacterPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    protected override bool IsVisibleInternal => false;
+
+    public override bool ShouldTakeExtraTurn(Player player) => player.Creature == Owner;
+    
+    public override Task AfterTakingExtraTurn(Player player)
+    {
+        return player.Creature == Owner ? PowerCmd.Decrement(this) : Task.CompletedTask;
+    }
+}

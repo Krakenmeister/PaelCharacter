b5deca6d730e29da7e20af467aefc2fca1b1bbf1
Krakenmeister <johndmeo@gmail.com>
2026-07-04 18:06:36 -0700

    Added Slick Back card

---
+++ PaelCharacterCode/Cards/Rare/Liquidate.cs
@@ -0,0 +1,26 @@
+﻿using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using PaelCharacter.PaelCharacterCode.Cards;
+
+namespace PaelCharacter.PaelCharacterCode.Cards.Rare;
+
+  
+public class Liquidate() : PaelCharacterCard(0,
+    CardType.Skill, CardRarity.Rare,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        
+    }
+
+    protected override void OnUpgrade()
+    {
+
+    }
+}
---
+++ PaelCharacterCode/Cards/Uncommon/SlickBack.cs
@@ -0,0 +1,45 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using PaelCharacter.PaelCharacterCode.Cards;
+using PaelCharacter.PaelCharacterCode.Extensions;
+using PaelCharacter.PaelCharacterCode.Utility;
+
+namespace PaelCharacter.PaelCharacterCode.Cards.Uncommon;
+
+public class SlickBack() : PaelCharacterCard(0,
+    CardType.Attack, CardRarity.Uncommon,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(7M, ValueProp.Move)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(PaelHoverTips.Wax)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        ArgumentNullException.ThrowIfNull(play.Target, "cardPlay.Target");
+        await DamageCmd
+            .Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this)
+            .Targeting(play.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+    }
+    
+    protected override PileType GetResultPileTypeForCardPlay()
+    {
+        PileType pileTypeForCardPlay = base.GetResultPileTypeForCardPlay();
+        if (this.IsWaxed())
+        {
+            return pileTypeForCardPlay != PileType.Discard ? pileTypeForCardPlay : PileType.Hand;
+        }
+
+        return pileTypeForCardPlay;
+    }
+
+    protected override void OnUpgrade() => DynamicVars.Damage.UpgradeValueBy(3M);
+}

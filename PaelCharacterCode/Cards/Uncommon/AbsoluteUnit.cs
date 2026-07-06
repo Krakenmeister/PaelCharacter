067d391ceb6ac422f449930d4a992362d46b1166
Krakenmeister <johndmeo@gmail.com>
2026-07-05 17:52:59 -0700

    Added Absolute Unit card

---
+++ PaelCharacterCode/Cards/Uncommon/AbsoluteUnit.cs
@@ -0,0 +1,24 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace PaelCharacter.PaelCharacterCode.Cards.Uncommon;
+
+public class AbsoluteUnit() : PaelCharacterCard(3,
+    CardType.Skill, CardRarity.Uncommon,
+    TargetType.Self)
+{
+    public override bool GainsBlock => true;
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(28M, ValueProp.Move)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, play);
+    }
+
+    protected override void OnUpgrade() => DynamicVars.Block.UpgradeValueBy(8);
+}
